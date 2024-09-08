6bb348c28a3e533877bfcd22a4c039b4e154571f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-08 13:00:54 +0200

    "Added Container component to App component"

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Heading } from "./components/Heading";
 import { Oscar } from "./components/Oscar";
 import { Button } from "./components/Button";
 import Input from "./components/Input";
+import { Container } from "./components/Container";
 
 function App() {
   const personName = {
@@ -45,6 +46,9 @@ function App() {
         handleClick={(event, id) => console.log("Button clicked", event, id)}
       />
       <Input value="" handleChange={(event) => console.log(event)} />
+      <Container
+        styles={{ border: "1px solid black", padding: "1rem", margin: "2rem" }}
+      />
     </div>
   );
 }
