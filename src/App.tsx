a853173ad05ce2bcc783b247302d6cf099a84038
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-08 14:49:46 +0200

    "Added User component import and usage in App component"

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Oscar } from "./components/Oscar";
 import { Button } from "./components/Button";
 import Input from "./components/Input";
 import { Container } from "./components/Container";
+import { User } from "./components/state/User";
 
 function App() {
   const personName = {
@@ -49,6 +50,7 @@ function App() {
       <Container
         styles={{ border: "1px solid black", padding: "1rem", margin: "2rem" }}
       />
+      <User />
     </div>
   );
 }
