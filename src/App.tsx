4a30c9302f562ea8e9041f652e5b813daf7ac08c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-08 11:54:37 +0200

    "Added Person component to App and passed personName object as prop"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Greet } from "./components/Greet";
+import Person from "./components/Person";
 
 function App() {
+  const personName = {
+    first: "Umut",
+    last: "Can",
+  };
   return (
     <div className="App">
       <Greet name="Umut" messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
